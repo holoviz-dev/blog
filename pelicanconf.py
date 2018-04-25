3c79b8b2b411dfaf99415751277ec47be379b9c6
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-04-25 17:07:36 +0100

    Initial commit adding blog and first post

---
+++ pelicanconf.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'PyViz Developers'
+SITENAME = 'HoloViews Blog'
+SITEURL = 'http://blog.holoviews.org'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/London'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+LINKS = (('HoloViews.org', 'https://holoviews.org/'),
+         ('GeoViews.org', 'https://geoviews.org/'),
+         ('PyViz.org', 'http://pyviz.org/'),)
+
+GITHUB_ORG = 'ioam'
+GITHUB_REPO = 'holoviews'
+TWITTER_USER = 'HoloViews'
+
+DEFAULT_PAGINATION = 10
+
+MARKUP = ('md', 'ipynb')
+
+PLUGIN_PATHS = ['./plugins']
+PLUGINS = ['ipynb.markup']
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+IGNORE_FILES = ['.ipynb_checkpoints']
+
+STATIC_PATHS = ['images']
+THEME = 'theme'
+
+LOGO = 'images/logo.png'
+
+IPYNB_USE_META_SUMMARY = True
+
+READERS = {"html": None}
